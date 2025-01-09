c6d5b0f9c42a7961d0c66669306725ebc18f97c2
ammaanm <137885530+ammaanm@users.noreply.github.com>
2025-01-09 22:04:46 +0000

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,37 @@ function App() {
   function onInput(e: InputEvent) {
     //@ts-ignore
     const inp: HTMLInputElement = e.target;
-    if (!(/^\d*$/g.test(inp.value))) {
-      const pos = inp.selectionStart || 0
-      const newVal = Array.from(inp.value.matchAll(/\d/g))?.join("") || ""
-      const newPos = pos - (inp.value.length - newVal.length)
+    if (!/^\d*$/g.test(inp.value)) {
+      const pos = inp.selectionStart || 0;
+      const newVal = Array.from(inp.value.matchAll(/\d/g))?.join("") || "";
+      const newPos = pos - (inp.value.length - newVal.length);
       inp.value = newVal;
-      inp.setSelectionRange(newPos, newPos)
+      inp.setSelectionRange(newPos, newPos);
       return;
     }
     if (inp.value.startsWith("0")) {
       inp.value = inp.value.replace(/^[0]+/g, "");
     }
-    setCount(Number.parseInt(inp.value)  || 0);
+    setCount(Number.parseInt(inp.value) || 0);
   }
 
   return (
     <>
       <PWABadge />
       {/* <div class="m-auto w-[80%]"> */}
-      <div class="w-fit m-auto text-[5vw]">
-        <label for="num-input" class="w-56">
-          Enter a number:{" "}
-        </label>
-        <input
-          type="text"
-          id="num-input"
-          maxLength={3}
-          class="m-auto w-[15vw] border-2 text-slate-600 text-center"
-          onInput={onInput}
-        />
+      <div class="m-4">
+        <div class="w-fit m-auto text-[5vw]">
+          <label for="num-input" class="w-56">
+            Enter a number:{" "}
+          </label>
+          <input
+            type="text"
+            id="num-input"
+            maxLength={3}
+            class="m-auto w-[15vw] border-2 text-slate-600 text-center"
+            onInput={onInput}
+          />
+        </div>
       </div>
       <br />
       <div class="m-auto w-fit">
