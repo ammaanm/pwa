a9b4f1df93afdba79ac197639b46a054f169e16c
ammaanm <137885530+ammaanm@users.noreply.github.com>
2025-01-09 17:50:07 +0000

    new file:   .gitignore 	modified:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.svg 	new file:   pwa-assets.config.ts 	new file:   src/App.tsx 	new file:   src/PWABadge.module.css 	new file:   src/PWABadge.tsx 	new file:   src/index.css 	new file:   src/index.tsx 	new file:   src/vite-env.d.ts 	new file:   tailwind.config.js 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { createSignal } from "solid-js";
+import PWABadge from "./PWABadge.tsx";
+
+function App() {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <>
+      <PWABadge />
+      <div class="m-auto w-[80%] border">
+        <input
+          type="number"
+          class="m-auto w-full border-2"
+          onInput={(e) => setCount(e.target.valueAsNumber)}
+        />
+        <br />
+        <div class="m-auto w-fit">
+          <b>{count()}</b> + <b>1</b> = <b>{count() + 1}</b>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import tailwindcss from "tailwindcss";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    solid(),
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "Increment",
+        short_name: "Inc",
+        description: "Enter a number, and add one to it!",
+        theme_color: "#c3c3c3",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: true,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+});
